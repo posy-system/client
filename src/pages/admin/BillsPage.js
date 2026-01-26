3b53143996c690a5cd7fc6f74f23ca145d9c17df
Manoj99 <141224046+BuddhikaManoj@users.noreply.github.com>
2026-01-26 23:31:20 +0530

    Admin bill UI and API connected

--- src/pages/admin/BillsPage.js
+++ src/pages/admin/BillsPage.js
@@ -1,12 +1,62 @@
 import React from "react";
+import TabelCompo, { TableFormat } from '../../components/tabel/TabelCompo';
+import toast from 'react-hot-toast';
+import { useState, useEffect } from 'react';
+import { GetBills, DeleteBill } from "../../apis/BillApis";
+
 
 export default function BillsPage() {
+  const [bills, setBills] = useState([]);
+
+  useEffect(() => {
+  const fetchBills = async () => {
+    try {
+      const response = await GetBills();
+      setBills(response.bills);
+    } catch (error) {
+      console.error("Error fetching bills:", error);
+    }
+  };
+  fetchBills();
+}, []);
+
+  const columns = [
+    {
+      key: "billNo",
+      header: "Bill No",
+    },
+    {
+      key: "imageUrl",
+      header: "Image",
+    },
+    {
+      key: "createdAt",
+      header: "Created",
+      render: (v) => TableFormat.formatDateTime(v),
+      muted: true,
+    },
+  ];
+
+  const handleDeleteBill = async (row) => {
+    try {
+      await DeleteBill(row._id);
+      setBills((prev) => prev.filter((r) => r._id !== row._id));
+      toast.success("Item deleted");
+    } catch (error) {
+      console.error("Error deleting item:", error);
+    }
+  };
+
   return (
     <div className="rounded-2xl border border-slate-200 bg-white p-5 shadow-sm dark:border-[#2A2A2C] dark:bg-[#19191A]">
-      <h2 className="text-lg font-semibold">Bills</h2>
-      <p className="mt-1 text-sm text-slate-600 dark:text-slate-400">
-        Bills page content goes here.
-      </p>
+      <div>
+        <TabelCompo
+          title="Bills"
+          rows={bills}
+          columns={columns}
+          onDelete={handleDeleteBill}
+        />
+      </div>
     </div>
   );
 }
